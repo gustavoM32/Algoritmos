ac11d897767c97273699353b9e06f1ff64b46d31
Gustavo de Medeiros Carlos <gustavo.10.dmc@gmail.com>
2019-09-03 20:36:43 -0300

    add ways to cover distance

--- DP/wcd.cpp
+++ DP/wcd.cpp
@@ -1,11 +1,31 @@
 /*
-    Nome do Algoritmo
+    Ways to Cover a Distance
     Complexity: O()
 */
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+#define MAXN 10010
+#define ll long long
 
+ll dp[MAXN];
+
+ll wcd(int dist) {
+    if (dp[dist] != -1) return dp[dist];
+    if (dist == 0) return dp[dist] = 1;
+    ll res = 0;
+    for (int i = 1; i <= 3; i++) {
+        if (dist >= i) {
+            res += wcd(dist - i);
+        }
+    }
+    return dp[dist] = res;
+}
+
+int main() {
+    int dist;
+    cin >> dist;
+    memset(dp, -1, sizeof dp);
+    cout << wcd(dist) << endl;
     return 0;
 }

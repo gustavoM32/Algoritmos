db1c004953890fe4450807b6998bf4f017f0b5d2
Gustavo de Medeiros Carlos <gustavo.10.dmc@gmail.com>
2019-09-03 22:17:43 -0300

    add subset sum problem

--- DP/ssp.cpp
+++ DP/ssp.cpp
@@ -1,11 +1,30 @@
 /*
-    Nome do Algoritmo
-    Complexity: O()
+    Subset Sum Problem
+    Complexity: O(n * sum)
 */
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+#define MAXN 1010
 
+int pd[MAXN][MAXN];
+
+bool ssp(int i, int sum, int arr[], int n) {
+    if (pd[i][sum] != -1) return pd[i][sum];
+    if (sum == 0) return pd[i][sum] = 1;
+    if (i == n) return pd[i][sum] = 0;
+    if (arr[i] > sum) {
+        return pd[i][sum] = ssp(i + 1, sum, arr, n);
+    }
+    return pd[i][sum] = ssp(i + 1, sum, arr, n) || ssp(i + 1, sum - arr[i], arr, n);
+}
+
+int main() {
+    int n, sum;
+    cin >> n >> sum;
+    int arr[MAXN];
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    memset(pd, -1, sizeof pd);
+    cout << (ssp(0, sum, arr, n) ? "YES" : "NO") << endl;
     return 0;
 }
